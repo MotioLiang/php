fa4224135c3b0496fde4ba369c253d7872b1a107
haoliang <1398814154@qq.com>
2017-10-10 15:56:12 +0800

    add file

---
+++ conditionals.php
@@ -0,0 +1,79 @@
+<?php 
+
+     # if & switch
+     # 关系运算符
+     # < > == != <= >= === !===
+     
+     // $num = 2;
+
+     #单个 if
+     #if ($num <= 4) echo "num的值一定是小于或者等于4";
+
+     #if else
+     // if ($num >= 4) {
+     //    echo "true";
+     // }else{
+     // 	echo "false";
+     // }
+
+     # nesting if
+     // $num = 4;
+     // if ($num >= 4) {
+     // 	if ($num <= 10) {
+     // 		echo "num一定是在4～10之间！";
+     // 	}else{
+     // 		echo "num大于10！";
+     // 	}
+     // }else{
+     // 		echo "num一定是小于4！";
+     // 	}
+     
+     # 逻辑运算符
+     /*
+         逻辑与 &&  AND
+         逻辑或 || OR
+         逻辑非 !
+         逻辑异或 XOR
+     */
+
+    $num = 3;
+    if ($num < 2 XOR $num > 10) {
+    	echo "num是大于3并且小于10";
+    }
+    /*
+        AND: 一假既假
+        OR： 一真既真
+        XOR: 只有一个为真，结果才为真
+    */
+
+    # switch
+    $favColor = 1;
+
+    switch ($favColor) {
+    	case '1':
+    		echo "angry";
+    		break;
+        case 'yellow':
+    		echo "happy";
+    		break;
+    	case 'blue':
+    		echo "silence";
+    		break;
+    	case 'green':
+    		echo "forgive";
+    		break;    	
+        default:
+            echo "GUN";
+            break;
+    }
+
+
+ ?>
+
+
+
+
+
+
+
+
